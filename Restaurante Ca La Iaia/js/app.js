aa9d3a32650b4b8a1c3032b54909f507e1e60ef5
paula8creatidev <paula.creatidev@gmail.com>
2025-09-16 18:41:30 +0200

    Add files via upload
    Added more AngularJS content to the html (index) and changed $scope names to fit spanish language

--- Restaurante Ca La Iaia/js/app.js
+++ Restaurante Ca La Iaia/js/app.js
@@ -2,62 +2,62 @@ var app = angular.module('miTienda', []);
 
 app.controller('MainController', ['$scope', function($scope)
 {
-    $scope.title = "Ca la Iaia";
-    $scope.subtitle = "Como lo hacía tu yaya";
-    $scope.products = 
+    $scope.titulo = "Ca la Iaia";
+    $scope.subtitulo = "Como lo hacía tu yaya";
+    $scope.productos = 
     [
         {
-            name: "Croquetas de Pollo",
-            image: "../img/croquetas.jpg",
-            price: 8.95,
+            nombre: "Croquetas de Pollo",
+            imagen: "../img/croquetas.jpg",
+            precio: 8.95,
             likes: 0
         },
         {
-            name: "Lasaña Boloñesa",
-            image: "../img/lasaña.jpg",
-            price: 11.99,
+            nombre: "Lasaña Boloñesa",
+            imagen: "../img/lasaña.jpg",
+            precio: 11.99,
             likes: 0
         },
         {
-            name: "Ravioli de la Yaya",
-            image: "../img/ravioli.jpg",
-            price: 13.75,
+            nombre: "Ravioli de la Yaya",
+            imagen: "../img/ravioli.jpg",
+            precio: 13.75,
             likes: 0
         },
         {
-            name: "Salmón a la Plancha",
-            image: "../img/salmon.jpg",
-            price: 15,
+            nombre: "Salmón a la Plancha",
+            imagen: "../img/salmon.jpg",
+            precio: 15,
             likes: 0
         },
         {
-            name: "Ensalada con Queso",
-            image: "../img/ensalada.jpg",
-            price: 12.75,
+            nombre: "Ensalada con Queso",
+            imagen: "../img/ensalada.jpg",
+            precio: 12.75,
             likes: 0
         },
         {
-            name: "Flan de Huevo",
-            image: "../img/flan.jpg",
-            price: 5,
+            nombre: "Flan de Huevo",
+            imagen: "../img/flan.jpg",
+            precio: 5,
             likes: 0
         },
         {
-            name: "Tarta de Naranja",
-            image: "../img/tarta.jpg",
-            price: 6.25,
+            nombre: "Tarta de Naranja",
+            imagen: "../img/tarta.jpg",
+            precio: 6.25,
             likes: 0
         },
         {
-            name: "Café",
-            image: "../img/coffee.jpg",
-            price: 1.20,
+            nombre: "Café",
+            imagen: "../img/coffee.jpg",
+            precio: 1.20,
             likes: 0
         },
         {
-            name: "Capuccino",
-            image: "../img/capuccino.jpg",
-            price: 2.1,
+            nombre: "Capuccino",
+            imagen: "../img/capuccino.jpg",
+            precio: 2.1,
             likes: 0
         }
     ]
